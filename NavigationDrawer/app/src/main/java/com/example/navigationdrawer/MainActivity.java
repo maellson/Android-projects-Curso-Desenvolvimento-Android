a5a15c5d30915cf6fdc717e7a4efd1c065ba83ac
maellson <maellson@hotmail.com>
2019-03-19 21:21:31 -0300

    fim do projeto Navigattion Drawer

--- NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.navigationdrawer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.navigationdrawer.fragments.CarteiraFragment;
+import com.example.navigationdrawer.fragments.PrincipalFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    //private FrameLayout  frameLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +32,18 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        PrincipalFragment principalFragment = new PrincipalFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.framContainer,principalFragment);
+        transaction.commit();
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "ACTION PARA ABRIR MAPS", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -75,22 +91,32 @@ public class MainActivity extends AppCompatActivity
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_account) {
+            PrincipalFragment principalFragment = new PrincipalFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-        } else if (id == R.id.nav_slideshow) {
+            transaction.replace(R.id.framContainer,principalFragment);
+            transaction.commit();
+            // Handle the camera action
+        } else if (id == R.id.nav_wallet) {
+            CarteiraFragment carteiraFragment = new CarteiraFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.framContainer,carteiraFragment);
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_coupom) {
 
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_chat) {
 
         }
 
